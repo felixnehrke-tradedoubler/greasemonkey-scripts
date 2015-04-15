be5086c5520a69c363b1f20ec37ab50a54edda4a
Felix Nehrke <f.nehrke@globit.com>
2015-04-16 01:13:57 +0200

    :cookie: Redesign the last column on a jira-board
    If you processed a lot of tickets in short time the last column tends
    to be overfilled by tickets. That creates an annoying long page scroll
    to find open tickets which are not rated so high.
    
    This change adds some CSS-Rules to redesign these tiles and make them
    tiny and easy to handle. This causes a minimal drop of information but
    this information should also be provided in the detailed view of the
    ticket.

--- src/clean-jira-email-tickets.user.js
+++ src/clean-jira-email-tickets.user.js
@@ -2,14 +2,53 @@
 // @name        Clean jira email-tickets
 // @namespace   jira
 // @description This Script resizes the inside images in gmail if they exceed the possible page width
-// @version     1.0
+// @version     1.1
 // @updateURL   https://raw.githubusercontent.com/nemoinho/greasemonkey-scripts/master/src/clean-jira-email-tickets.user.js
 // @downloadURL https://raw.githubusercontent.com/nemoinho/greasemonkey-scripts/master/src/clean-jira-email-tickets.user.js
 // @include     http://192.168.168.11:8081/browse/*
 // @include     https://192.168.168.11:8081/browse/*
+// @include     http://192.168.168.11:8081/secure/RapidBoard.jspa*
+// @include     https://192.168.168.11:8081/secure/RapidBoard.jspa*
 // @grant       GM_addStyle
 // ==/UserScript==
 
+GM_addStyle(
+    ".ghx-columns li:last-child .ghx-issue-fields,"
+    + ".ghx-columns li:last-child .ghx-issue-content,"
+    + ".ghx-columns li:last-child .ghx-issue-content"
+    + "{"
+    + "    font-size:11px;"
+    + "    height:auto;"
+    + "    margin:0;"
+    + "    min-height:0;"
+    + "    max-height:16px;"
+    + "    overflow:hidden;"
+    + "    padding:1px 8px 1px 3px;"
+    + "}"
+    + ".ghx-columns li:last-child .ghx-key,"
+    + ".ghx-columns li:last-child .ghx-summary,"
+    + ".ghx-columns li:last-child .ghx-summary .ghx-inner"
+    + "{ display:inline; }"
+    + ".ghx-columns li:last-child .ghx-summary .ghx-inner:before"
+    + "{ content:' ';display:inline; }"
+    + ".ghx-columns li:last-child .ghx-flags,"
+    + ".ghx-columns li:last-child .ghx-days,"
+    + ".ghx-columns li:last-child .ghx-flags,"
+    + ".ghx-columns li:last-child .ghx-type"
+    + "{ display:none; }"
+    + ".ghx-columns li:last-child .ghx-avatar {"
+    + "    height:16px;"
+    + "    line-height:16px;"
+    + "    padding:0 1px;"
+    + "    right:0;"
+    + "    top:0;"
+    + "}"
+    + ".ghx-columns li:last-child .ghx-avatar-img {"
+    + "    height:16px;"
+    + "    line-height:16px;"
+    + "    width:16px;"
+    + "}"
+);
 
 var cleanUp = function(){
     var ticketContent = document.querySelectorAll('#description-val .user-content-block p');
