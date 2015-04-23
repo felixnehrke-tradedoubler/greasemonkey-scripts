c2b6394c3ffd16d7a759b7bf1cd4dcf0906b3531
Felix Nehrke <f.nehrke@globit.com>
2015-04-23 11:57:01 +0200

    :scissors: Keep br-tags in the ticket-text
    The br-tags where removed to make the tickets more readable if they
    where created by mail. But this behavior is not relevant anymore and
    reduce the readability of many other tickets.

--- src/clean-jira-email-tickets.user.js
+++ src/clean-jira-email-tickets.user.js
@@ -2,7 +2,7 @@
 // @name        Clean jira email-tickets
 // @namespace   jira
 // @description This Script resizes the inside images in gmail if they exceed the possible page width
-// @version     1.1
+// @version     1.2
 // @updateURL   https://raw.githubusercontent.com/nemoinho/greasemonkey-scripts/master/src/clean-jira-email-tickets.user.js
 // @downloadURL https://raw.githubusercontent.com/nemoinho/greasemonkey-scripts/master/src/clean-jira-email-tickets.user.js
 // @include     http://192.168.168.11:8081/browse/*
@@ -49,17 +49,3 @@ GM_addStyle(
     + "    width:16px;"
     + "}"
 );
-
-var cleanUp = function(){
-    var ticketContent = document.querySelectorAll('#description-val .user-content-block p');
-    for (var i = ticketContent.length; i--; ) {
-        var node = ticketContent[i];
-        var br = node.getElementsByTagName('br');
-        for (var j = br.length; j--; ) {
-            node.removeChild(br[j]);
-        }
-    }
-}
-
-cleanUp();
-window.setInterval(cleanUp, 9e3);
