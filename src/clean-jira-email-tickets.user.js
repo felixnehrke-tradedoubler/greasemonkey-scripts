941bee085b4b3b137d5312dcd3cb98f6923eb3e3
Felix Nehrke <felix.nehrke@tradedoubler.com>
2015-11-04 13:05:07 +0100

    :sparkles: Show more meta information in Jira-Ticket
    Show some more meta-informations of a done-ticket:
    - The ticket-type
    - The priority
    - The estimation
    
    Nevertheless updated the script-description and added a more generic url

--- src/clean-jira-email-tickets.user.js
+++ src/clean-jira-email-tickets.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
+// ==UserScript==
 // @name        Clean jira email-tickets
 // @namespace   jira
-// @description This Script resizes the inside images in gmail if they exceed the possible page width
-// @version     1.3
+// @description Arange the done-column of the jira-board neatly.
+// @version     1.4
 // @updateURL   https://raw.githubusercontent.com/nemoinho/greasemonkey-scripts/master/src/clean-jira-email-tickets.user.js
 // @downloadURL https://raw.githubusercontent.com/nemoinho/greasemonkey-scripts/master/src/clean-jira-email-tickets.user.js
-// @include     http://192.168.171.11:8081/browse/*
-// @include     https://192.168.171.11:8081/browse/*
-// @include     http://192.168.171.11:8081/secure/RapidBoard.jspa*
-// @include     https://192.168.171.11:8081/secure/RapidBoard.jspa*
+// @include     http://jira.*/secure/RapidBoard.jspa*
+// @include     https://jira.*/secure/RapidBoard.jspa*
 // @grant       GM_addStyle
 // ==/UserScript==
 
@@ -21,9 +20,9 @@ GM_addStyle(
     + "    height:auto;"
     + "    margin:0;"
     + "    min-height:0;"
-    + "    max-height:16px;"
+    + "    max-height:32px;"
     + "    overflow:hidden;"
-    + "    padding:1px 8px 1px 3px;"
+    + "    padding:1px 16px 1px 3px;"
     + "}"
     + ".ghx-columns li:last-child .ghx-key,"
     + ".ghx-columns li:last-child .ghx-summary,"
@@ -31,11 +30,22 @@ GM_addStyle(
     + "{ display:inline; }"
     + ".ghx-columns li:last-child .ghx-summary .ghx-inner:before"
     + "{ content:' ';display:inline; }"
-    + ".ghx-columns li:last-child .ghx-flags,"
-    + ".ghx-columns li:last-child .ghx-days,"
-    + ".ghx-columns li:last-child .ghx-flags,"
-    + ".ghx-columns li:last-child .ghx-type"
+    + ".ghx-columns li:last-child .ghx-days"
     + "{ display:none; }"
+    + ".ghx-columns li:last-child .ghx-flags"
+    + "{"
+    + "    left:20px;"
+    + "    top:1px;"
+    + "}"
+    + ".ghx-columns li:last-child .ghx-type"
+    + "{"
+    + "    left:5px;"
+    + "    top:1px;"
+    + "}"
+    + ".ghx-columns li:last-child .ghx-key"
+    + "{"
+    + "    padding-left:30px;"
+    + "}"
     + ".ghx-columns li:last-child .ghx-avatar {"
     + "    height:16px;"
     + "    line-height:16px;"
@@ -47,5 +57,20 @@ GM_addStyle(
     + "    height:16px;"
     + "    line-height:16px;"
     + "    width:16px;"
+    + "}"
+    + ".ghx-columns li:last-child .ghx-end {"
+    + "    background:transparent;"
+    + "    box-shadow:none;"
+    + "    bottom:auto;"
+    + "    top:0;"
+    + "    right:18px;"
+    + "}"
+    + ".ghx-columns li:last-child .ghx-end .ghx-corner {"
+    + "    line-height:1;"
+    + "    font-size:14px;"
+    + "}"
+    + ".ghx-columns li:last-child .ghx-end .ghx-corner .aui-badge {"
+    + "    padding:2px;"
+    + "    font-size:11px;"
     + "}"
 );
